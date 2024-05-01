b89c22836453a4ba1f209ae569660f9d9b42a5db
Adwaith Nair <adwaith1310@gmail.com>
2024-05-01 12:58:34 +0530

    added user output schema

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,18 @@
 from pydantic import BaseModel,EmailStr
 from datetime import datetime
 
+#schema to create user
 class UserCreate(BaseModel):
     email:EmailStr
     username:str
     password:str
+
+#schema for user output
+class UserOut(BaseModel):
+    id:int
+    email:EmailStr
+    username:str
+    created_at:datetime
+    
+    class Config:
+        orm_mode = True

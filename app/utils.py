4eb53e40d4a5493e85c08fde70af8ffee4757ac7
Adwaith Nair <adwaith1310@gmail.com>
2024-05-02 11:28:23 +0530

    added function to verify password

--- app/utils.py
+++ app/utils.py
@@ -4,4 +4,8 @@ pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
 #password hash function
 def hash(password:str):
     return pwd_context.hash(password)
+
+#verify password
+def verify(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
     

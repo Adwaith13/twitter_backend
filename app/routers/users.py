b8ca1188553adf200b9bce45266e1c9dc953a543
Adwaith Nair <adwaith1310@gmail.com>
2024-05-03 13:18:19 +0530

    added delete user endpoint

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,10 +1,11 @@
 #importing all necessary packages
-from fastapi import APIRouter,status,HTTPException,Depends
+from fastapi import APIRouter,status,HTTPException,Depends,Response
 from sqlalchemy.orm import Session
 from ..database import get_db
 from ..models import User
 from ..schemas import UserCreate,UserOut
 from ..utils import hash
+from ..oauth2 import get_current_user
 
 #create a router
 router = APIRouter(prefix="/users",tags=["Users"])
@@ -31,4 +32,24 @@ def get_user(id:int,db:Session=Depends(get_db)):
                             detail=f"User with {id} does not exist")
         
     return user
+
+#delete user
+@router.delete("/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(id:int,db:Session=Depends(get_db),
+                 current_user = Depends(get_current_user)):
+    user_query = db.query(User).filter(User.id == id)
+    user = user_query.first()
+    
+    if user == None:
+         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id {id} does not exist")
+         
+    if user.id != current_user.id:
+         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Not authorized to perform requested action")
+         
+    user_query.delete(synchronize_session = False)
+    db.commit()
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)  
     

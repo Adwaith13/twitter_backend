9fe0311ca1f5ba010dff58a8aa1e51031ca42b16
Adwaith Nair <adwaith1310@gmail.com>
2024-05-03 13:01:42 +0530

    added tweet delete endpoint

--- app/routers/tweet.py
+++ app/routers/tweet.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Depends,HTTPException,status
+from fastapi import APIRouter,Depends,HTTPException,status,Response
 from sqlalchemy.orm import Session
 from ..models import Tweets
 from ..oauth2 import get_current_user
@@ -11,8 +11,8 @@ router = APIRouter(prefix="/tweets", tags=["Tweets"])
 #create new tweet
 @router.post("/",response_model=TweetGet,status_code=status.HTTP_201_CREATED)
 def create_tweet(tweet:CreateTweet,db:Session=Depends(get_db),
-                 current_user_id:int = Depends(get_current_user)):
-    new_tweet = Tweets(owner_id = current_user_id.id,**tweet.dict())
+                 current_user:int = Depends(get_current_user)):
+    new_tweet = Tweets(owner_id = current_user.id,**tweet.dict())
     db.add(new_tweet)
     db.commit()
     db.refresh(new_tweet)
@@ -36,3 +36,22 @@ def get_all_tweets(db:Session=Depends(get_db)):
     tweet = db.query(Tweets).all()
     return tweet
 
+#delete tweet
+@router.delete("/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_tweet(id:int,db:Session=Depends(get_db),
+                 current_user = Depends(get_current_user)):
+    tweet_query = db.query(Tweets).filter(Tweets.id == id)
+    tweet = tweet_query.first()
+    
+    if tweet == None:
+         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist")
+         
+    if tweet.owner_id != current_user.id:
+         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Not authorized to perform requested action")
+         
+    tweet_query.delete(synchronize_session = False)
+    db.commit()
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)    
